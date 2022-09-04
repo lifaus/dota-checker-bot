2d506736b8869ff3d7783a22f69d0751bdcafe52
Mark <112800067+nouseruk@users.noreply.github.com>
2022-09-04 15:55:41 +0000

    Test functional

--- src/bot/MessagesChecker.php
+++ src/bot/MessagesChecker.php
@@ -2,6 +2,7 @@
 
 class MessagesChecker {
     public Telegram $api;
+    public OpenDota $dota_api;
 
     public ?int $user_id = null;
     public ?int $chat_id = null;
@@ -9,6 +10,7 @@ class MessagesChecker {
 
     public function __construct(array $message_object, Telegram $api) {
         $this->api = $api;
+        $this->dota_api = new OpenDota;
         $this->message_object = $message_object;
     }
 
@@ -16,10 +18,31 @@ class MessagesChecker {
         $this->user_id = $this->message_object['message']['from']['id'];
         $this->chat_id = $this->message_object['message']['chat']['id'];
         $text_message = $this->message_object['message']['text'];
+        $text_message_split = explode(" ", $text_message);
 
-        if ($text_message == "/start") {
-            $this->api->sendMessage("test", $this->chat_id);
+        switch ($text_message_split[0]) {
+            case '/start':
+                $this->api->sendMessage("Hello, I am a bot for dota 2 account information or match details.\n\nFor a list of commands: /help", $this->chat_id);
+            break;
+            
+            case '/help':
+                $this->api->sendMessage("Commands list:\n\n/player [SteamID32] - get information about dota account\n/match [matchID] - get information about the match", $this->chat_id);
+            break;
+
+            case '/player':
+                $player = $this->dota_api->getPlayerInfo($text_message_split[1]);
+                $wl = $this->dota_api->getPlayerInfo($text_message_split[1], "wl");
+                $text = "Avatar: {$player['profile']['avatarfull']}\nApproximate rating: {$player['mmr_estimate']['estimate']} MMR\nWin/Lose: ✅ {$wl['win']} ❌{$wl['lose']}";
+                $this->api->sendMessage($text, $this->chat_id);
+            break;
+
+            case '/match':
+                //TODO
+            break;
+                
         }
+
+        
 
     }
 }
