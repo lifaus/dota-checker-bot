6794814b9bf96a1ddac43adb9ce26446e4b5d908
Mark Willis <nouseruk@gmail.com>
2022-09-05 17:57:21 +0000

    refactore(player command): added new functional
    + Add win/lose statistic
    + Fix bug if args[1] string

--- src/bot/MessagesChecker.php
+++ src/bot/MessagesChecker.php
@@ -30,10 +30,13 @@ class MessagesChecker {
             break;
 
             case '/player':
+                if (empty($text_message_split[1])) {
+                    return $this->api->sendMessage("Usage: /player [Player ID|STEAMID32]", $this->chat_id);
+                }
                 //Get player data using OpenDota API
                 $player = $this->dota_api->getPlayerInfo($text_message_split[1]);
                 if (!isset($player['profile'])) {
-                    return $this->api->sendPhoto("🔒 Player not found", new Imagick("./assets/backgrounds/not_found_profile.png"), $this->chat_id);
+                    return $this->api->sendPhoto("🔒 This profile is private.\nDota 2 player match data is private by default", new Imagick("./assets/backgrounds/lock_profile.png"), $this->chat_id);
                 }
                 //Get Rank Tier
                 $rank = $player['rank_tier'];
@@ -41,6 +44,24 @@ class MessagesChecker {
 
                 //Create picture
                 $im = new Imagick("./assets/backgrounds/background.png");
+                $draw = new ImagickDraw();
+                $draw->setFillColor('white');
+                $draw->setFont("./assets/fonts/montserrat-alternates-semibold.ttf");
+                $draw->setFontSize(45);
+
+                $im->annotateImage($draw, 350, 350, 0, $player['profile']['personaname']);
+                //Get win & lose
+                $wl = $this->dota_api->getPlayerInfo($text_message_split[1], "wl");
+                $draw->setFontSize(30);
+
+                //win
+                $draw->setFillColor('#75ff78');
+                $im->annotateImage($draw, 350, 390, 0, "Win: ".$wl['win']);
+
+                //lose
+                $draw->setFillColor('#ff4f4f');
+                $im->annotateImage($draw, 350, 430, 0, "Lost: ".$wl['lose']);
+
                 if (!is_null($rank)) {
                     $x = 910; $y = 275;
                     $rank_icon = new Imagick("./assets/rank_icons/rank_icon_{$rank[0]}.png");
@@ -57,14 +78,10 @@ class MessagesChecker {
                 $im->compositeImage($avatar, Imagick::COMPOSITE_DEFAULT, 130, 270);
 
                 $this->api->sendPhoto("👤 Player {$player['profile']['personaname']}\n🆔 SteamID: {$player['profile']['steamid']}", $im, $this->chat_id);
-
-                //$wl = $this->dota_api->getPlayerInfo($text_message_split[1], "wl");
-                //$text = "Avatar: {}\nApproximate rating: {$player['mmr_estimate']['estimate']} MMR\nWin/Lose: ✅ {$wl['win']} ❌{$wl['lose']}";
-                //$this->api->sendMessage($text, $this->chat_id);
             break;
 
             case '/match':
-                
+                //TODO
             break;
                 
         }
